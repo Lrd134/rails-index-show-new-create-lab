44802c27adce4861a1e9669fc81df47491879744
Lrd134 <larryc3200@gmail.com>
2021-07-02 01:01:08 -0400

    commit tree 21addf39f22576e34d78c9efdd65f7ee6b7a0e5e

---
+++ app/controllers/coupons_controller.rb
@@ -0,0 +1,38 @@
+class CouponsController < ApplicationController
+
+  # GET /coupons
+  # GET /coupons.json
+  def index
+    @coupons = Coupon.all
+  end
+
+  # GET /coupons/1
+  # GET /coupons/1.json
+  def show
+    @coupon = Coupon.find_by(id: params[:id])
+  end
+
+  # GET /coupons/new
+  def new
+    @coupon = Coupon.new
+  end
+
+  # GET /coupons/1/edit
+  def edit
+  end
+
+  # POST /coupons
+  # POST /coupons.json
+  def create
+    @coupon = Coupon.new
+
+    @coupon.coupon_code = params[:coupon][:coupon_code]
+    @coupon.store = params[:coupon][:store]
+    @coupon.save
+    redirect_to coupon_path(@coupon)
+  end
+
+
+
+
+end
